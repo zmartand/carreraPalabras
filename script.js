a75f5de505d98f67e1650e7dd7a2c875733fa5ca
Zara Martín <zmartand@myuax.es>
2024-04-20 22:14:45 +0200

    js inicio base

--- script.js
+++ script.js
@@ -1,19 +1,7 @@
-//Atributos y constantes
-const palabras = ["mar", "cirujano", "transportar", "programa", "ironhack", "universal", "microscopio", "quiosco", "positivo", "cine", "comer", "sal", "canguro", "maravilloso", "perro", "ingeniero", "colmena", "escritor"] //Array de palabras random
-let palabraActual = 0;
-let nivel = 1; //Empezamos en el nivel 1
-let vidas = 3; //Inicialmente empezamos con 3 vidas
-let duracionPalabra = 8000; //Duración de la palabra inicialmente en milisegundos
-let rondas = 0; //Para contar las rondas jugadas
+const palabrasArray = ["gato", "maravilloso", "javascript", "oceano", "sol", "espacio", "programar"];
+let palabraUsuario;
+palabras = palabrasArray[Math.floor(Math.random()*palabrasArray.length)];
 
-const minDuracionPalabra = 2000; //Mínima duracion de la palabra
-const contadorNivel = document.getElementById("nivel");
-const contadorVidas = document.getElementById("vidas");
-const inputUsuario =document.getElementById("usuarioInput");
-const instruccionesJuego = document.getElementById("instrucciones");
-const resultadoJuego = document.getElementById("resultado");
-
-function actualizarDetalles(){
-    contadorNivel.textContent = nivel;
-    contadorVidas.textContent = vidas;
+const inicio = () => { //1
+    document.getElementById('palabra').textContent = palabras;
 }
